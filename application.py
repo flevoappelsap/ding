7f2382a8bcc0561e0cb07bb6866bae26d200b69f
flevoappelsap <61715028+flevoappelsap@users.noreply.github.com>
2020-03-07 19:18:59 +0100

    Add files via upload

---
+++ application.py
@@ -0,0 +1,53 @@
+import os
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+import random
+import sqlite3
+import csv
+import requests
+import time
+from helpers import apology, login_required
+import json
+
+# Configure application
+app = Flask(__name__)
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+
+
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def start():
+    return render_template("index.html")
+
+@app.route("/status", methods=["GET", "POST"])
+def status():
+    a = request.form.get("status")
+    print(a)
+    print("i")
+    return render_template("status.html")
